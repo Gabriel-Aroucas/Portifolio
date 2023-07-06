b825467333fe9c13ca5f976e82374e7e7a61f34e
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-07-05 22:25:31 -0300

    remove the category unnecessary

--- js/sobreMim.js
+++ js/sobreMim.js
@@ -3,3 +3,19 @@ let typed = new Typed('#title', {
     typeSpeed: 50,
 });
 
+
+let openCloseProjects = 0;
+let content = document.querySelector(".content");
+const projects = document.querySelector(".projects");
+
+
+projects.addEventListener('click', () => {
+    if(openCloseProjects == 0){
+      content.style.display = 'block';
+      openCloseProjects++;
+    }else if(openCloseProjects == 1){
+      content.style.display = 'none';
+      openCloseProjects--;
+    }
+  })
+
