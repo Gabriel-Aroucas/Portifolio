68023b0c2155fb79d238d930fe44c9b483825f1b
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-07-05 22:05:27 -0300

    added more projects on section challenges

---
+++ js/challenges.js
@@ -0,0 +1,21 @@
+let typed = new Typed('#title', {
+    strings: ['^500 Desafios do FrontedMEntor.io'],
+    typeSpeed: 50,
+});
+
+
+let openCloseProjects = 0;
+let content = document.querySelector(".content");
+const projects = document.querySelector(".projects");
+
+
+projects.addEventListener('click', () => {
+    if(openCloseProjects == 0){
+      content.style.display = 'block';
+      openCloseProjects++;
+    }else if(openCloseProjects == 1){
+      content.style.display = 'none';
+      openCloseProjects--;
+    }
+  })
+
